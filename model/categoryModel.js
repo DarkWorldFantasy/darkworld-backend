955486e7c6504c750f995db01f95ee99f0a3b886
bdphotoshot <50048375+bdphotoshot@users.noreply.github.com>
2023-05-24 22:12:15 +0600

    Delete model directory

--- model/categoryModel.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const categorySchema = new Schema({
-    name: { type: String, minLength: 3, unique: true, required: true },
-});
-
-const categoryModel = model("category", categorySchema);
-
-module.exports = categoryModel;
