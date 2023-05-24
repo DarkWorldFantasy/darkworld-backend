c37a85a5e0f4d7cc93db17653c840616ed679210
bdphotoshot <50048375+bdphotoshot@users.noreply.github.com>
2023-05-24 22:12:31 +0600

    Delete router directory

--- router/userRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const userController = require("../controller/userController");
-
-router.route("/signIn").post(userController.signIn);
-router.route("/signUp").post(userController.signUp);
-
-module.exports = router;
