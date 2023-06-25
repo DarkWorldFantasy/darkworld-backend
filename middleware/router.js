9dfb2f5759ca432f304e4bd2f14376f217229569
bdphotoshot <50048375+bdphotoshot@users.noreply.github.com>
2023-06-25 20:01:35 +0600

    Update router.js

--- middleware/router.js
+++ middleware/router.js
@@ -1,7 +1,6 @@
 const userRouter = require("../router/userRouter");
 const categoryRouter = require("../router/categoryRouter");
 const providerRouter = require("../router/providerRouter");
-const productRouter = require("../router/productRouter");
 const entertainerRouter = require("../router/entertainerRouter");
 const cartRouter = require("../router/cartRouter");
 const orderRouter = require("../router/orderRouter");
@@ -11,7 +10,6 @@ module.exports = (app) => {
     app.use("/user/", userRouter);
     app.use("/category", categoryRouter);
     app.use("/provider", providerRouter);
-    app.use("/product", productRouter);
     app.use("/entertainer", entertainerRouter);
     app.use("/cart", cartRouter);
     app.use("/order", orderRouter);
