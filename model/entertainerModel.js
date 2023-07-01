803b9f9a6ffc1e2fa417429b506c22218ccdc1b3
bdphotoshot <50048375+bdphotoshot@users.noreply.github.com>
2023-07-01 20:09:41 +0600

    Update entertainerModel.js

--- model/entertainerModel.js
+++ model/entertainerModel.js
@@ -5,6 +5,7 @@ const entertainerSchema = new Schema({
     username: { type: String, minLength: 3, unique: true, required: true },
     religion: { type: String, minLength: 3, required: true },
     tribe: { type: Boolean, required: true },
+    boobs: { type: String, default: "medium", required: true },
     category: { type: Schema.Types.ObjectId, ref: "category", required: true },
     availability: { type: Boolean, default: true },
     profile: { data: Buffer, contentType: String },
