8288c12edaf4dd90207fbe1bdeb6bdb596218080
bdphotoshot <50048375+bdphotoshot@users.noreply.github.com>
2023-07-01 20:21:30 +0600

    Add files via upload

---
+++ router/entertainerRouter.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const entertainerController = require("../controller/entertainerController");
+const authorize = require("../middleware/authorize");
+
+router
+    .route("/filter")
+    .post(authorize, entertainerController.getFilterEntertainer);
+router
+    .route("/")
+    .get(authorize, entertainerController.getEntertainer)
+    .post(authorize, entertainerController.createEntertainer);
+router
+    .route("/:id")
+    .get(authorize, entertainerController.getEntertainerById)
+    .put(authorize, entertainerController.updateEntertainer)
+    .delete(authorize, entertainerController.deleteEntertainer);
+
+router.route("/getProfile/:id").get(entertainerController.getProfliePhoto);
+
+module.exports = router;
