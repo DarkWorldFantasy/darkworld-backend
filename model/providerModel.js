93f4c586cc1aac1e860c4c0e253935fc66c7df34
bdphotoshot <50048375+bdphotoshot@users.noreply.github.com>
2023-10-28 00:09:58 +0600

    Delete model/providerModel.js

--- model/providerModel.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const providerSchema = new Schema({
-    name: { type: String, minLength: 3, unique: true, required: true },
-});
-
-const providerModel = model("provider", providerSchema);
-
-module.exports = providerModel;
